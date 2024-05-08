6cff156ecf8f8643a92c6e2292ca7b780e0eed58
Yousef <85886365+YR973@users.noreply.github.com>
2024-05-08 22:34:50 +0300

    fixed the search error
    fixed an error where the web app needed to be restarted in order to search again

--- Test/Test/PythonClass.cs
+++ Test/Test/PythonClass.cs
@@ -5,142 +5,80 @@ using System.Diagnostics;
 using System.IO;
 using Test.Pages;
 using System.Linq;
+using Microsoft.VisualBasic.CompilerServices;
+using Org.BouncyCastle.Utilities;
+
 namespace Test
 {
 
     public class PythonClass
     {
-        // public static string[] RunScript(string query)
-        // {
-        //     Console.WriteLine("before engine called");
-        //     if (!PythonEngine.IsInitialized)
-        //     {
-        //         Runtime.PythonDLL = @"..\Python311\python311.dll";
-        //         
-        //     }
-        //     PythonEngine.Initialize();
-        //     Console.WriteLine("after engine called");
-        //     
-        //     using (Py.GIL()) 
-        //     {
-        //         Console.WriteLine("before script called2");
-        //         string scriptPath = @"..\Test\Pages";
-        //         string modulePath = @"..\Test\modulespy";
-        //         string scriptName = "llmneuralsearch";
-        //         dynamic sys = Py.Import("sys");
-        //         sys.path.extend(".");
-        //         sys.path.append(modulePath);
-        //         sys.path.append(scriptPath);
-        //         var pythonScript = Py.Import(scriptName);
-        //         Console.WriteLine("after script called2");
-        //         var pyquery = new PyString(query);
-        //         Console.WriteLine("before search called");
-        //         var result = pythonScript.InvokeMethod("search", new PyObject[] {pyquery}).ToString().Trim('[', ']').Split(',');
-        //         Console.WriteLine("after search called");
-        //         //Py.GIL().Dispose();
-        //         //PythonEngine.Initialize(false);
-        //         return result;
-        //     }
-        //     
-        // }
-
-        // public static string[] RunScript(string query)
-        // {
-        //     if (!(PythonEngine.IsInitialized))
-        //     {
-        //         Runtime.PythonDLL = @"..\Python311\python311.dll";
-        //         PythonEngine.Initialize();
-        //         string[] result;
-        //         string scriptPath = @"..\Test\Pages";
-        //         string modulePath = @"..\Test\modulespy";
-        //         string scriptName = "llmneuralsearch";
-        //         dynamic sys = Py.Import("sys");
-        //         sys.path.extend(".");
-        //         sys.path.append(modulePath);
-        //         sys.path.append(scriptPath);
-        //         var pythonScript = Py.Import(scriptName);
-        //         var pyquery = new PyString(query);
-        //         result = pythonScript.InvokeMethod("search", new PyObject[] { pyquery })
-        //             .ToString().Trim('[', ']')
-        //             .Split(',');
-        //         Console.WriteLine("after search called");
-        //         return result;
-        //     }
-        //     else
-        //     {
-        //         string[] result;
-        //         string scriptName = "llmneuralsearch";
-        //         var pythonScript = Py.Import(scriptName);
-        //         var pyquery = new PyString(query);
-        //         result = pythonScript.InvokeMethod("search", new PyObject[] { pyquery })
-        //             .ToString().Trim('[', ']')
-        //             .Split(',');
-        //         
-        //         return result;
-        //     }
-        // }
-        
-        
         public static string[] RunScript(string query)
         {
             if (!PythonEngine.IsInitialized)
             {
-                Runtime.PythonDLL = @"..\Python311\python311.dll";
-            }
-            Console.WriteLine("before engine called");
-            
-            PythonEngine.Initialize();
-            Console.WriteLine("after engine called");
-            using (Py.GIL()) 
-            {
-                Console.WriteLine("before script called2");
                 string scriptPath = @"..\Test\Pages";
                 string modulePath = @"..\Test\modulespy";
-                string scriptName = "llmneuralsearch";
+                PythonEngine.Initialize();
                 dynamic sys = Py.Import("sys");
-                sys.path.extend(".");
-                sys.path.append(modulePath);
-                sys.path.append(scriptPath);
-                var pythonScript = Py.Import(scriptName);
-                Console.WriteLine("after script called2");
-                var pyquery = new PyString(query);
-                Console.WriteLine("before search called");
-                var result = pythonScript.InvokeMethod("search", new PyObject[] {pyquery}).ToString().Trim('[', ']').Split(',');
-                Console.WriteLine("after search called");
-                
-                return result;
+                sys
+                    .path
+                    .extend(".");
+                sys
+                    .path
+                    .append(modulePath);
+                sys
+                    .path
+                    .append(scriptPath);
             }
-            
+            //string scriptName = "llmneuralsearch";
+            var pythonScript = Py.Import("llmneuralsearch");
+            var pyquery = new PyString(query);
+            var r = pythonScript.InvokeMethod("search", new PyObject[] { pyquery });
+            //dynamic pyStr = Py.Import("builtins").GetAttr("str");
+            var resultArray = 
+                Py
+                    .Import("builtins")
+                    .GetAttr("str")
+                    .Invoke(r)
+                    .As<string>()
+                    .Trim('[', ']')
+                    .Split(',');
+            return resultArray;
         }
         
         public static string[] Suggest(string query)
         {
             if (!PythonEngine.IsInitialized)
             {
-                Runtime.PythonDLL = @"..\Python311\python311.dll";
+                string scriptPath = @"..\Test\Pages";
+                string modulePath = @"..\Test\modulespy";
                 PythonEngine.Initialize();
-            }
-            using (Py.GIL())
-            {
-                string scriptPath = @"Test\Pages";
-                string modulePath = @"Test\modulespy";
-                string scriptName = "llmsuggestions";
                 dynamic sys = Py.Import("sys");
-                sys.path.extend(".");
-                sys.path.append(modulePath);
-                sys.path.append(scriptPath);
-                var pythonScript = Py.Import(scriptName);
-                var pyquery = new PyString(query);
-                var result = pythonScript.InvokeMethod("search", new PyObject[] {pyquery}).ToString().Trim('[', ']').Split(',');
-                return result;
-                
+                sys
+                    .path
+                    .extend(".");
+                sys
+                    .path
+                    .append(modulePath);
+                sys
+                    .path
+                    .append(scriptPath);
             }
-
-            
+            //string scriptName = "llmsuggestions";
+            var pythonScript = Py.Import("llmsuggestions");
+            var pyquery = new PyString(query);
+            var r = pythonScript.InvokeMethod("search", new PyObject[] { pyquery });
+            //dynamic pyStr = Py.Import("builtins").GetAttr("str");
+            var resultArray = 
+                Py.Import("builtins")
+                    .GetAttr("str")
+                    .Invoke(r)
+                    .As<string>()
+                    .Trim('[', ']')
+                    .Split(',');
+            return resultArray;
+                
         }
-        
-        
     }
-    
-
 }
--- Test/Test/obj/Debug/net7.0/Test.AssemblyInfo.cs
+++ Test/Test/obj/Debug/net7.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb16116ae95d86882b19b54c467c87d685be1109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c747eeb2d5e516d0347bb64bb94940eb997a814")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
